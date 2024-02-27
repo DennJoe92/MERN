1181a964046ea5125a8073eb06072f4ee86398f3
DennJoe92 <153688190+DennJoe92@users.noreply.github.com>
2024-02-27 13:55:21 +0000

    MERN TUtorial - Lesson 3 Round Routes Creation

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,22 @@ require('dotenv').config()
 
 //This line tells the file that Express.js is required to run this file//
 const express = require('express')
+//This points to the Rounds Routes file
+const roundRoutes = require('./routes/Rounds')
 
 //This then runs the express app
 const app = express ()
 
+//Middlewear that logs data to database
+app.use(express.json())
 //Middlewear that logs out the request and response 
 app.use((req, res, next) =>{
     console.log(req.path, req.method)
     next()
 })
 
-//Route Handler to respond to a get request and log a response
-app.get ('/', (req, res) => {
-    res.json({mssg: 'Welcome'})
-})
+//Route Handler pointing to routes set up in Rounds file
+app.use('/api/Rounds',roundRoutes)
 
 //Listen for requests on a certain port and then log them to the console
 app.listen (process.env.PORT, () =>{
