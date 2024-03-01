1617006524a3916e531586f9ff3a6d489dcd22d0
DennJoe92 <153688190+DennJoe92@users.noreply.github.com>
2024-03-01 16:57:20 +0000

    MERN Tutorial 9 - Round Output Component, Home and CSS

---
+++ frontend/src/components/roundDetails.js
@@ -0,0 +1,11 @@
+const RoundDetails = ({ round }) => {
+    return (
+        <div className="round-details">
+            <h4>{round.course}</h4>
+            <p><strong>Players:</strong>{round.player}</p>
+            <p><strong>Score:</strong>{round.score}</p>
+            <p>{round.date}</p>
+        </div>
+    )
+}
+export default RoundDetails
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
+
+//import components
+import RoundDetails from "../components/roundDetails"
+
 const Home = () => {
+    const [rounds, setRounds] = useState(null)
+    
+    useEffect(() => {
+        const fetchRounds = async () => {
+            const response = await fetch('/api/rounds')
+            const json = await response.json()
+
+            if (response.ok) {
+                setRounds(json)
+            }
+        }
+
+        fetchRounds()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="Rounds">
+                {rounds && rounds.map((round) => (
+                     <RoundDetails round={round} key={round._id} />
+                    
+                ))}
         </div>
+    </div>
     )
-}
-
+                }
 export default Home
